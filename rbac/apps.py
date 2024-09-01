caa8a93dfebf3af305c7221c197a0e4d8c375f41
redatman <evicwork@gmail.com>
2024-09-02 04:27:52 +0800

    feat(rbac): introduce signal to initialize rbac system
    This commit introduces a new signal for initializing permissions after migrations. The signal `init` is connected to the `post_migrate` signal and calls a dedicated function to perform the initialization.
    
    This change simplifies the process of initializing permissions and ensures that they are always correctly configured after migrations.

--- rbac/apps.py
+++ rbac/apps.py
@@ -4,15 +4,17 @@ from __future__ import print_function, unicode_literals
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
-from rbac.management import create_permissions
-
 
 class RbacConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'rbac'
     verbose_name = 'RBAC'
 
-    def ready(self):
-        post_migrate.connect(create_permissions,
-            dispatch_uid='rbac.management.create_permissions'
-        )
+    # def ready(self):
+    #     # post_migrate.connect(create_permissions,
+    #     #     dispatch_uid='rbac.management.create_permissions'
+    #     # )
+
+    #     from rbac.signals import init
+
+    #     post_migrate.connect(init, sender=self)
