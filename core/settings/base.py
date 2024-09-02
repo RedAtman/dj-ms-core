d8553a2b34eac383f2ed4554e444960900b7eccb
redatman <evicwork@gmail.com>
2024-09-03 04:51:51 +0800

    feat: Integrate cuser for user management
    Integrates the cuser package for user management, including:
    
    - Adding cuser to INSTALLED_APPS
    - Adding CuserMiddleware to MIDDLEWARE
    
    This enhances user management capabilities by leveraging the features of cuser.

--- core/settings/base.py
+++ core/settings/base.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third party apps
+    'cuser',
     'whitenoise.runserver_nostatic',
     'corsheaders',
     'rest_framework',
@@ -104,6 +105,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cuser.middleware.CuserMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
