050f878338c57cd0b2f1e9fca09f356fababf09a
redatman <evicwork@gmail.com>
2024-09-13 00:41:57 +0800

    Fix: Configure AuditLog and Celery settings
    Refine Celery settings for improved performance and stability.
    
    Configure AuditLog to track changes across the application.
    
    This commit addresses issues with Celery configuration and introduces audit logging to improve application monitoring and security.

--- core/settings/guardian.py
+++ core/settings/guardian.py
@@ -4,5 +4,5 @@ INSTALLED_APPS.insert(-1, 'guardian')  # noqa: F405
 
 AUTHENTICATION_BACKENDS += [  # noqa: F405
     'django.contrib.auth.backends.ModelBackend', # this is default
-    'guardian.backends.ObjectPermissionBackend',
+    'guardian.backends.ObjectPermissionBackend'
 ]
